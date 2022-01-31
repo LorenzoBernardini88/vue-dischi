437236c6901e43e7d62a33cbc12c7e5b29942fac
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-01-31 15:12:19 +0100

    creo components Header e Main

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    
+    <Header/>
   </div>
 </template>
 
 <script>
-
+import Header from './components/macro/Header.vue';
 
 export default {
+  
   name: 'App',
   components: {
-  
+    Header
   }
 }
 </script>
 
 <style lang="scss">
-
+@import url('./assets/globals.scss');
 </style>
---
+++ src/components/Macro/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="logo_cont">
+        <img src="" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/style/vars.scss';
+.logo_cont {
+    background: $color2;
+    padding: 10px 20px;
+}
+
+</style>
